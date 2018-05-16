aae2dc6e732cb18fca405b1efd80233169f397fe
Rani Zilpelwar <rani@8thlight.com>
2018-05-16 15:20:02 -0400

    dry up request header code

--- tic_tac_toe_client.rb
+++ tic_tac_toe_client.rb
@@ -15,16 +15,8 @@ class TicTacToeClient
   end
 
   def match_types
-    uri = URIGenerator.new.uri("/match_types")
-
-    http = Net::HTTP.new(uri.host, uri.port)
-
-    request = Net::HTTP::Get.new(uri)
-    request["Content-Type"] = 'application/json'
-    request["Cache-Control"] = 'no-cache'
-
-    response = http.request(request)
-    response.read_body
-    #puts "match_types response = #{response.read_body}"
+    route_string = "/match_types"
+    request = RequestGenerator.new.put(route_string, data)
+    response = ResponseRetriever.new.response(route_string, request)
   end
 end
