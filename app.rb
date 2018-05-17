67c694a9a486a84ecbb239ff725436c58c07adf2
Rani Zilpelwar <rani@8thlight.com>
2018-05-17 10:53:57 -0400

    Added match_selection_prompt message to intro page

--- app.rb
+++ app.rb
@@ -8,9 +8,15 @@ require_relative './helpers/response/response_parser.rb'
 set :port, 8080
 
 get '/' do
+  @title = "Tic Tac Toe"
   response = TicTacToeClient.new.message("en", "welcome")
   @intro_text = ResponseParser.parse(response, "message", "text")
-  @title = "Tic Tac Toe"
+
+  response = TicTacToeClient.new.message("en", "match_selection_prompt")
+  @match_selection_prompt = ResponseParser.parse(response, "message", "text")
+
+  response = TicTacToeClient.new.match_types
+  @match_types = ResponseParser.parse(response, nil, "matches")
   erb :intro, :layout => :layout
 end
 
