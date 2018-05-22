5ce94b32db16cb520be7923b46d0d0bef200a004
Rani Zilpelwar <rani@8thlight.com>
2018-05-22 16:52:36 -0400

    one big step closer to getting the json from the response

--- public/request/request_generator.js
+++ public/request/request_generator.js
@@ -1,22 +1,26 @@
 var request_generator = function(){
   var xmlHttp = new XMLHttpRequest();
   var url = "http://localhost:4567/match_types";
+  var data = "";
   
   xmlHttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      var data = JSON.parse(this.responseText);
-      myFunction(data);
+      data = this.response;
+      get_data(data);
     }
   };
 
   xmlHttp.open("GET", url, true);
-  
+  xmlHttp.responseType = "json";
   xmlHttp.setRequestHeader("Content-Type", "application/json");
   
   xmlHttp.send();
 };
 
-function myFunction(data) {
+function get_data(data) {
   console.log("myFunction contents");
   console.log("response data = " + data);
+  console.log("response length = " + data.length);
+  console.log(data);
+  return data;
 };
