b0cae218e627bc392d5b0e132ae7358693691ed3
Rani Zilpelwar <rani@8thlight.com>
2018-05-22 14:07:54 -0400

    insert js code into base html doc for start page of game

--- public/start.js
+++ public/start.js
@@ -1,2 +1,8 @@
 document.title = 'Tic Tac Toe';
-alert('Hello, World!');
+var match_selection_prompt_para = document.createElement("p");
+
+var match_selection_prompt_node = document.createTextNode("match_selection_prompt");
+match_selection_prompt_para.appendChild(match_selection_prompt_node);
+
+var insertionPoint = document.getElementById("start_game");
+insertionPoint.appendChild(match_selection_prompt_para);
