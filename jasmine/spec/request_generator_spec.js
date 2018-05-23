188729b9e89319d737bff64f87f6ed81a96667eb
Rani Zilpelwar <rani@8thlight.com>
2018-05-23 10:35:35 -0400

    use jasmine test to try to understand how async requests work, test still fails because the data comes back after the timeout

--- jasmine/spec/request_generator_spec.js
+++ jasmine/spec/request_generator_spec.js
@@ -1,10 +1,49 @@
-describe("Request Generator", function() {
-  it("should create a get request for match_types", function() {
-    var result = request_generator();
-    console.log("result = " + result);
-    //var request = result.get("/match_types");
-    //var content = request.body;
-    var expected_result_for_matches = '{\"matches\":[{\"player1_type\":\"Human\",\"player2_type\":\"Human\"},{\"player1_type\":\"Human\",\"player2_type\":\"Computer\"},{\"player1_type\":\"Computer\",\"player2_type\":\"Computer\"}],\"errors\":{\"error_message\":\"\"}}';
-    expect(result).toEqual(expected_result_for_matches);
-  });
+var results = "initial value";
+var get_results = function(data){
+  results = data;
+  console.log("inside test callback function:");
+  console.log(data);
+  console.log(data["matches"][0]["player1_type"]);
+  console.log("results:");
+  console.log(results);
+};
+
+// Code under test
+var flag = false;
+console.log("initial flag value:");
+console.log(flag);
+
+function testAsync(done) {
+    // Wait two seconds, then set the flag to true
+    console.log("Default timeout = " + jasmine.DEFAULT_TIMEOUT_INTERVAL);
+    setTimeout(function () {
+        flag = true;
+        console.log("inside testAsync");
+        request_generator(get_results);
+        // Invoke the special done callback
+        done();
+        console.log("after testAsync done");
+    }, jasmine.DEFAULT_TIMEOUT_INTERVAL);
+}
+
+// Specs
+describe("Testing async calls with beforeEach and passing the special done callback around", function () {
+    console.log("beforeeach start:")
+    beforeEach(function (done) {
+      console.log("beforeeach 1");
+        // Make an async call, passing the special done callback        
+        testAsync(done);
+      console.log("beforeeach end");
+      console.log("flag value = " + flag)
+      console.log("results:");
+      console.log(results);
+    });
+
+    it("Should be true if the async call has completed", function () {
+        console.log("before expect, flag value = " + flag);
+        console.log("before expect, results value = ");
+        console.log(results);
+        expect(flag).toEqual(true);
+    });
+
 });
