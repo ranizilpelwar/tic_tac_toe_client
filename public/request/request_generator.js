73564adce8a4e7d8ca2daa413e62c23bdbaacced
Rani Zilpelwar <rani@8thlight.com>
2018-05-23 10:45:38 -0400

    Remove logging code from method

--- public/request/request_generator.js
+++ public/request/request_generator.js
@@ -1,9 +1,6 @@
 var request_generator = function(route_string, callback){
   var xmlHttp = new XMLHttpRequest();
   var url = uri_generator(route_string).uri();
-  console.log("url = ");
-  console.log(url);
-  var data;
   
   xmlHttp.open("GET", url, true);
   xmlHttp.responseType = "json";
@@ -13,12 +10,7 @@ var request_generator = function(route_string, callback){
 
   xmlHttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      console.log("in readyState")
       callback(this.response);
     }
   };
-
-  console.log("data after sending the request:");
-  console.log(data);
-  
 };
