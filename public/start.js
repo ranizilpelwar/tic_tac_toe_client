a0d285dc787f88c9817a3dd2e646a9f8f7049ef7
Rani Zilpelwar <rani@8thlight.com>
2018-05-23 14:29:05 -0400

    Use a for loop so that the text array will print each value depending on how many elements are in it

--- public/start.js
+++ public/start.js
@@ -1,12 +1,17 @@
 document.title = 'Tic Tac Toe';
 
 var display_match_selection_prompt_text = function(response_data){
-  var match_selection_prompt_para = document.createElement("p");
-  var match_selection_prompt_text = response_data["message"]["text"][0];
-  var match_selection_prompt_node = document.createTextNode(match_selection_prompt_text);
-  match_selection_prompt_para.appendChild(match_selection_prompt_node);
   var insertionPoint = document.getElementById("start_game");
-  insertionPoint.appendChild(match_selection_prompt_para);
+  var text_array = response_data["message"]["text"];
+  var index;
+  var match_selection_prompt_node;
+  var match_selection_prompt_para;
+  for(index = 0; index < text_array.length; index++) {
+    match_selection_prompt_node = document.createTextNode(text_array[index]);
+    match_selection_prompt_para = document.createElement("p");
+    match_selection_prompt_para.appendChild(match_selection_prompt_node);
+    insertionPoint.appendChild(match_selection_prompt_para);
+  }
 };
 
 var message_properties = {
@@ -16,4 +21,5 @@ var message_properties = {
 
 var message_data_to_request = message(message_properties).data();
 
-put_request("/message_content", display_match_selection_prompt_text, message_data_to_request );
+put_request("/message_content", display_match_selection_prompt_text, message_data_to_request);
+
