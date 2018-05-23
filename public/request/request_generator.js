090f81224ea6189b7e3bf0e9e54ae8608166cd1c
Rani Zilpelwar <rani@8thlight.com>
2018-05-23 10:36:01 -0400

    use function callback to do the work for the request once the response is received, asynchronously

--- public/request/request_generator.js
+++ public/request/request_generator.js
@@ -1,26 +1,22 @@
-var request_generator = function(){
+var request_generator = function(callback){
   var xmlHttp = new XMLHttpRequest();
   var url = "http://localhost:4567/match_types";
-  var data = "";
+  var data;
   
-  xmlHttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      data = this.response;
-      get_data(data);
-    }
-  };
-
   xmlHttp.open("GET", url, true);
   xmlHttp.responseType = "json";
   xmlHttp.setRequestHeader("Content-Type", "application/json");
   
   xmlHttp.send();
-};
 
-function get_data(data) {
-  console.log("myFunction contents");
-  console.log("response data = " + data);
-  console.log("response length = " + data.length);
+  xmlHttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      console.log("in readyState")
+      callback(this.response);
+    }
+  };
+
+  console.log("data after sending the request:");
   console.log(data);
-  return data;
+  
 };
