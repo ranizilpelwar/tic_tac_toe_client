fd685163beeb06e277721ebff29fd5515fa1e571
Rani Zilpelwar <rani@8thlight.com>
2018-05-24 13:45:48 -0400

    Construct start page using promises

--- public/start.js
+++ public/start.js
@@ -19,18 +19,44 @@ message_properties = {
 
 var convertedData = message(message_properties).data();
 
-put("/message_content", convertedData).then(function(responseData){
+put("/message_content", convertedData)
+.then(function(responseData){
   displayMessage(responseData, "start_game");
-}, function(error){console.error("PUT message_content welcome Failed");})
+}, function(error){console.error("Display message_content welcome Failed");})
+.then(function(){
+  message_properties["type"] = "match_selection_prompt";
+  convertedData = message(message_properties).data();
+  return put("/message_content", convertedData);
+}, function(error) {console.error("PUT message_content match_selection_prompt Failed");})
+.then(function(responseData){
+  displayMessage(responseData, "start_game");
+}, function(error) {console.error("Display message_content match_selection_prompt Failed");})
 .then(function(){
   return get("/match_types");
 }, function(error) {console.error("GET match_types Failed");})
 .then(function(responseData){
   console.log("GET match_types Success");
   displayMatchTypes(responseData, "start_game");
-}, function(error) {console.error("GET match_types Failed");}
-);
-
+}, function(error) {console.error("Display match_types Failed");})
+.then(function(){
+  message_properties["type"] = "player_setup_prompt";
+  convertedData = message(message_properties).data();
+  return put("/message_content", convertedData);
+}, function(error) {console.error("PUT message_content player_setup_prompt Failed");})
+.then(function(responseData){
+  displayMessage(responseData, "start_game");
+}, function(error) {console.error("Display message_content player_setup_prompt Failed");})
+.then(function(){
+  message_properties["type"] = "player_symbol_option";
+  convertedData = message(message_properties).data();
+  return put("/message_content", convertedData);
+}, function(error) {console.error("PUT message_content player_symbol_option Failed");})
+.then(function(responseData){
+  displayMessage(responseData, "start_game");
+}, function(error) {console.error("Display message_content player_symbol_option Failed");})
+.then(function(){
+  display_player_input_boxes("start_game");
+}, function(error) {console.error("Display player_input_boxes Failed");});
 
 
 
