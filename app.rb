8240104650a7071c0167e4db78eed17af172aaba
Rani Zilpelwar <rani@8thlight.com>
2018-05-30 09:23:12 -0400

    default route redirects to index.html file

--- app.rb
+++ app.rb
@@ -1,29 +1,8 @@
 require 'sinatra'
 require 'sinatra/reloader'
-require 'json'
-require_relative './tic_tac_toe_client.rb'
-require_relative './helpers/response/response_parser.rb'
-require_relative './routes/game.rb'
 
 set :port, 8080
 
 get '/' do
-  @title = "Tic Tac Toe"
-  response = TicTacToeClient.new.message("en", "welcome")
-  @intro_text = ResponseParser.parse(response, "message", "text")
-
-  response = TicTacToeClient.new.message("en", "match_selection_prompt")
-  @match_selection_prompt = ResponseParser.parse(response, "message", "text")
-
-  response = TicTacToeClient.new.match_types
-  @match_types = ResponseParser.parse(response, nil, "matches")
-
-  response = TicTacToeClient.new.message("en", "player_setup_prompt")
-  @player_setup_prompt = ResponseParser.parse(response, "message", "text")
-
-  response = TicTacToeClient.new.message("en", "player_symbol_option")
-  @player_symbol_option = ResponseParser.parse(response, "message", "text")
-
-  erb :intro, :layout => :layout
+  File.read(File.join('public', 'index.html'))
 end
-
