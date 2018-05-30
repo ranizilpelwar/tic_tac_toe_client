a463f8293e758749cd27a2f1fcf120e0570b58c6
Rani Zilpelwar <rani@8thlight.com>
2018-05-30 14:35:40 -0400

    Added post request

--- public/request/request_generator_promise.js
+++ public/request/request_generator_promise.js
@@ -40,3 +40,25 @@ function put(route_string, json_data_to_send) {
     request.send(json_data_to_send);
   });
 };
+
+function post(route_string, json_data_to_send) {
+  return new Promise(function(resolve, reject) {
+    var request = new XMLHttpRequest();
+    request.responseType = "json";
+    var url = uri_generator(route_string).uri();
+    request.open("POST", url);
+    request.setRequestHeader("Content-Type", "application/json");
+    request.onload = function() {
+      if(request.status == 200){
+        resolve(request.response);
+      }
+      else {
+        reject(Error(request.statusText));
+      }
+    };
+    request.onerror = function() {
+      reject(Error("Network Error"));
+    };
+    request.send(json_data_to_send);
+  });
+};
