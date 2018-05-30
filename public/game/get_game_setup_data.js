a02a35ec48d40a0743a3e5a44bf53baae9549803
Rani Zilpelwar <rani@8thlight.com>
2018-05-30 14:36:08 -0400

    Created function that retrieves element data from the DOM

---
+++ public/game/get_game_setup_data.js
@@ -0,0 +1,23 @@
+function getGameSetupData(){
+  let matchNumber = "";
+  let firstPlayerSymbol = "";
+  let secondPlayerSymbol = "";
+
+  let gameSetupData = document.getElementsByTagName("input");
+  
+  firstPlayerSymbol = gameSetupData["player1_symbol"].value;
+  secondPlayerSymbol = gameSetupData["player2_symbol"].value;
+
+  let matches = Array.from(gameSetupData);
+  let selectedMatch = matches.filter(x => x.type === "radio" && x.checked === true);
+  matchNumber = selectedMatch[0].value;
+  console.log("matchNumber = " + matchNumber);
+  console.log("firstPlayerSymbol = " + firstPlayerSymbol);
+  console.log("secondPlayerSymbol = " + secondPlayerSymbol);
+
+  return {
+    "matchNumber": matchNumber,
+    "firstPlayerSymbol": firstPlayerSymbol,
+    "secondPlayerSymbol": secondPlayerSymbol
+  }
+}
