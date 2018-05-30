1438f92d17130242c6348e26f79fcd1d94b63bd6
Rani Zilpelwar <rani@8thlight.com>
2018-05-30 16:37:21 -0400

    Add input boxes to game details page

--- public/game/display_game_details.js
+++ public/game/display_game_details.js
@@ -20,24 +20,52 @@ function displayGameDetails(elementNameOfInsertionPoint, allMessages){
 
     match_number = responseData["game"]["match_number"]
     index = match_number - 1;
-    let updatedMessageText = playersIntroTemplate.replace("[1]", responseData["game"]["player1_symbol"].toUpperCase());
-    updatedMessageText = updatedMessageText.replace("[2]", allMessages["matches"][index]["player1_type"]);
-    updatedMessageText = updatedMessageText.replace("[3]", responseData["game"]["player2_symbol"].toUpperCase());
-    updatedMessageText = updatedMessageText.replace("[4]", allMessages["matches"][index]["player2_type"]);
+    let player1_symbol = responseData["game"]["player1_symbol"].toUpperCase();
+    let player2_symbol = responseData["game"]["player2_symbol"].toUpperCase();
+    let player1_type = allMessages["matches"][index]["player1_type"];
+    let player2_type = allMessages["matches"][index]["player2_type"];
+    let currentPlayerSymbol = responseData["game"]["current_player_symbol"].toUpperCase();
+    
+    let updatedMessageText = playersIntroTemplate.replace("[1]", player1_symbol);
+    updatedMessageText = updatedMessageText.replace("[2]", player1_type);
+    updatedMessageText = updatedMessageText.replace("[3]", player2_symbol);
+    updatedMessageText = updatedMessageText.replace("[4]", player2_type);
 
     insertText(gameDetailsContainer, updatedMessageText);
 
-    parent.appendChild(gameDetailsContainer);
-
-    //player value label or "" if first time
-
     //Board label
+    insertText(gameDetailsContainer, allMessages["messages"]["board_intro"]);
 
     //Display board
 
     //current player prompt
+    let nextMoveTemplate = allMessages["messages"]["next_move_prompt"];
+    updatedMessageText = nextMoveTemplate.replace("[1]", currentPlayerSymbol);
+    insertText(gameDetailsContainer, updatedMessageText);
 
+    //player input boxes - enabled or disabled based on player type and if player is current player
+    let inputText1 = "Player " + player1_symbol + ":";
+    let id1 = "player1_input";
+    input1 = displayInput(gameDetailsContainer, inputText1, id1);
+
+    let br = document.createElement("BR");
+    gameDetailsContainer.appendChild(br);
+
+    let inputText2 = "Player " + player2_symbol + ":";
+    let id2 = "player2_input";
+    input2 = displayInput(gameDetailsContainer, inputText2, id2);
+
+    if (currentPlayerSymbol === player1_symbol) {
+      input2.disabled = true;
+    } else {
+      input1.disabled = true;
+    }
     //submit button
+
+
+    parent.appendChild(gameDetailsContainer);
+
+
       }, function(error){console.error("Post game: Failed." + error);}
   );
 }
--- public/player/display_input_boxes.js
+++ public/player/display_input_boxes.js
@@ -17,3 +17,13 @@ var displayPlayerSymbolInputs = function(parentElement) {
   }
   parentElement.appendChild(table);
 };
+
+var displayInput = function(parentElement, inputText, inputId){
+  let textNode = document.createTextNode(inputText);
+  let input = document.createElement('input');
+  input.setAttribute("type", "text");
+  input.setAttribute("id", inputId);
+  parentElement.appendChild(textNode);
+  parentElement.appendChild(input);
+  return input;
+};
