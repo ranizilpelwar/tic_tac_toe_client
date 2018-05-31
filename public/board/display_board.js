f209b3b9528edd18767ad48d7280dcdb8419c0f4
Rani Zilpelwar <rani@8thlight.com>
2018-05-31 09:59:43 -0400

    Added id to board table/row/data so that css will apply a border to this table only

--- public/board/display_board.js
+++ public/board/display_board.js
@@ -1,10 +1,13 @@
 var displayBoard = function(parentElement, responseData) {
   let table = document.createElement('table');
+  table.setAttribute("id", "board");
   let boardIndex = 0;
   for (var row = 1; row <= 3; row++){
     let tableRow = document.createElement('tr');
+    tableRow.setAttribute("id", "board");
     for (var column = 1; column <= 3; column++){
       let tableData = document.createElement('td');
+      tableData.setAttribute("id", "board");
       let textNode = document.createTextNode(responseData.game.board[boardIndex]);
       tableData.appendChild(textNode);
       tableRow.appendChild(tableData);
