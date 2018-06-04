c3d371918353a2cd9b6ef25865b9c1fb628651c9
Rani Zilpelwar <rani@8thlight.com>
2018-06-04 16:15:13 -0400

    Rearrage _form elements so that the form can be removed upon starting game

--- public/game/display_game_initialization_form.js
+++ public/game/display_game_initialization_form.js
@@ -1,32 +1,32 @@
 var displayGameInitializationForm = function(){
   elementNameOfInsertionPoint = "start_game";
   
+  let gameContentParent = document.createElement("div");
+  gameContentParent.setAttribute("id", "initialization_content");
+
   let form = document.createElement("form");
   form.setAttribute("id", "start_game_form");
   form.setAttribute("method", "post");
   form.setAttribute("action", "javascript:;");
   form.onsubmit = function() {createGame("start_game");};
-  
-  let gameContentParent = document.createElement("div");
-  gameContentParent.setAttribute("id", "initialization_content");
 
-  insertText(gameContentParent, applicationMessages["messages"]["welcome"]);
-  insertText(gameContentParent, applicationMessages["messages"]["match_selection_prompt"]);
+  gameContentParent.appendChild(form);
+
+  insertText(form, applicationMessages["messages"]["welcome"]);
+  insertText(form, applicationMessages["messages"]["match_selection_prompt"]);
   
   let matchTypes = document.createElement('div');
   matchTypes.setAttribute("id", "match_types");
   displayMatchTypes(matchTypes, applicationMessages);
-  gameContentParent.appendChild(matchTypes);
+  form.appendChild(matchTypes);
 
-  insertText(gameContentParent, applicationMessages["messages"]["player_setup_prompt"]);
-  insertText(gameContentParent, applicationMessages["messages"]["player_symbol_option"]);
+  insertText(form, applicationMessages["messages"]["player_setup_prompt"]);
+  insertText(form, applicationMessages["messages"]["player_symbol_option"]);
   
-  displayPlayerSymbolInputs(gameContentParent);
+  displayPlayerSymbolInputs(form);
   
-  displaySubmitButton(gameContentParent, "start_game_submit", "Let's get started!");
+  displaySubmitButton(form, "start_game_submit", "Let's get started!");
   
-  form.appendChild(gameContentParent);
-
   let insertionPoint = document.getElementById(elementNameOfInsertionPoint);
-  insertionPoint.appendChild(form);
+  insertionPoint.appendChild(gameContentParent);
 };
