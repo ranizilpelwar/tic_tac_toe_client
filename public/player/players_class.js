e7df6297bf6446ff1fd9995ec424da9f3424efcc
Rani Zilpelwar <rani@8thlight.com>
2018-06-05 11:47:46 -0400

    Create a players class that stores the players in the game and their type, which aids in visual display of players at the top of the game and when to click the Go button depending on player type

---
+++ public/player/players_class.js
@@ -0,0 +1,41 @@
+class Players {
+  constructor(gameSetupData){
+    let matchNumber = gameSetupData["matchNumber"];
+    let playerType1 = applicationMessages["matches"][matchNumber -1]["player1_type"];
+    let playerType2 = applicationMessages["matches"][matchNumber -1]["player2_type"];
+
+    if (gameSetupData["selectedFirstPlayerSymbol"] === gameSetupData["firstPlayerSymbol"]){
+      this.player1Symbol = gameSetupData["firstPlayerSymbol"];
+      this.player1Type = playerType1;
+      this.currentPlayerSymbol = this.player1Symbol;
+      this.currentPlayerType = this.player1Type;
+      this.player2Symbol = gameSetupData["secondPlayerSymbol"];
+      this.player2Type = playerType2;
+    }
+    else if (gameSetupData["selectedFirstPlayerSymbol"] === gameSetupData["secondPlayerSymbol"]){
+      this.player1Symbol = gameSetupData["secondPlayerSymbol"];
+      this.player1Type = playerType2;
+      this.currentPlayerSymbol = this.player1Symbol;
+      this.currentPlayerType = this.player1Type;
+      this.player2Symbol = gameSetupData["firstPlayerSymbol"];
+      this.player2Type = playerType1;
+    }
+    else throw new PlayersException("selectedFirstPlayerSymbol doesn't match players");
+  }
+
+  refreshCurrent(currentPlayerSymbol){
+    if (currentPlayerSymbol === this.player1Symbol){
+      this.currentPlayerType = player1Type;
+      this.currentPlayerSymbol = currentPlayerSymbol;
+    }
+    else if (currentPlayerSymbol === player2Symbol){
+      this.currentPlayerType = player2Type;
+      this.currentPlayerSymbol = currentPlayerSymbol;
+    }
+    else throw new PlayersException("currentPlayerSymbol doesn't match players");
+  }
+
+  toString(){
+    return "player1Symbol: " + this.player1Symbol + ", player1Type: " + this.player1Type + ", player2Symbol: " + this.player2Symbol + ", player2Type: " + this.player2Type + ", currentPlayerSymbol: " + this.currentPlayerSymbol + "."; 
+  }
+}
