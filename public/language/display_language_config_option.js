d9be7bb4b62e8ab1d320b028a57491f391464161
Rani Zilpelwar <rani@8thlight.com>
2018-06-06 16:59:52 -0400

    Add beginnings of language configuration screen

--- public/language/display_language_config_option.js
+++ public/language/display_language_config_option.js
@@ -5,7 +5,10 @@ var displayLanguageConfigOption = function(parentElement){
   let languageTextElement = insertText(languageConfigDiv, languageConfigText);
   languageTextElement.setAttribute("id", "language_config_text");
   let languageButton = displaySubmitButton(languageTextElement, "language_config_submit", "Configure Language");
-  languageButton.onclick = function() { //configure language screen
+  languageButton.onclick = function() { 
+    let div = document.getElementById("start_game");
+    let parent = removeExistingContent(div);
+    displayLanguageOptionsSelectionContent(parent);
   };
   parentElement.appendChild(languageConfigDiv);
 };
