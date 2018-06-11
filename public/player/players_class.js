065cbb5f9a37fe7b16e112392461ce9d290faa55
Rani Zilpelwar <rani@8thlight.com>
2018-06-10 21:36:18 -0400

    add player number to player model as it is used often in other methods to identify it

--- public/player/players_class.js
+++ public/player/players_class.js
@@ -1,10 +1,11 @@
 class Players {
-  constructor(gameSetupData){
+  constructor(gameSetupData) {
     let matchNumber = gameSetupData["matchNumber"];
     let playerType1 = applicationMessages["matches"][matchNumber -1]["player1_type"];
     let playerType2 = applicationMessages["matches"][matchNumber -1]["player2_type"];
+    this.currentPlayerNumber = 1;
 
-    if (gameSetupData["selectedFirstPlayerSymbol"] === gameSetupData["firstPlayerSymbol"]){
+    if (gameSetupData["selectedFirstPlayerSymbol"] === gameSetupData["firstPlayerSymbol"]) {
       this.player1Symbol = gameSetupData["firstPlayerSymbol"].toUpperCase();
       this.player1Type = playerType1;
       this.currentPlayerSymbol = this.player1Symbol;
@@ -12,7 +13,7 @@ class Players {
       this.player2Symbol = gameSetupData["secondPlayerSymbol"].toUpperCase();
       this.player2Type = playerType2;
     }
-    else if (gameSetupData["selectedFirstPlayerSymbol"] === gameSetupData["secondPlayerSymbol"]){
+    else if (gameSetupData["selectedFirstPlayerSymbol"] === gameSetupData["secondPlayerSymbol"]) {
       this.player1Symbol = gameSetupData["secondPlayerSymbol"].toUpperCase();
       this.player1Type = playerType2;
       this.currentPlayerSymbol = this.player1Symbol;
@@ -23,21 +24,27 @@ class Players {
     else throw new PlayersException("selectedFirstPlayerSymbol doesn't match players");
   }
 
-  refreshCurrent(currentPlayerSymbol){
+  refreshCurrent(currentPlayerSymbol) {
     currentPlayerSymbol = currentPlayerSymbol.toUpperCase();
     console.log("players refreshCurrent currentPlayerSymbol = " + currentPlayerSymbol);
-    if (currentPlayerSymbol === this.player1Symbol){
+    if (currentPlayerSymbol === this.player1Symbol) {
       this.currentPlayerSymbol = currentPlayerSymbol;
       this.currentPlayerType = this.player1Type;
+      this.currentPlayerNumber = 1;
     }
     else if (currentPlayerSymbol === this.player2Symbol){
       this.currentPlayerSymbol = currentPlayerSymbol;
       this.currentPlayerType = this.player2Type;
+      this.currentPlayerNumber = 2;
     }
     else throw new PlayersException("currentPlayerSymbol doesn't match players");
   }
 
-  toString(){
-    return "player1Symbol: " + this.player1Symbol + ", player1Type: " + this.player1Type + ", player2Symbol: " + this.player2Symbol + ", player2Type: " + this.player2Type + ", currentPlayerSymbol: " + this.currentPlayerSymbol + "."; 
+  getPlayerNumber() {
+    return this.currentPlayerNumber;
+  }
+
+  toString() {
+    return "player1Symbol: " + this.player1Symbol + ", player1Type: " + this.player1Type + ", player2Symbol: " + this.player2Symbol + ", player2Type: " + this.player2Type + ", currentPlayerSymbol: " + this.currentPlayerSymbol + ", currentPlayerType: " + this.currentPlayerType + ", currentPlayerNumber: " + this.currentPlayerNumber; 
   }
 }
