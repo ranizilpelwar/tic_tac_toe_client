fd7280c6f5bb277e04e40a86783e84dfa555e802
Rani Zilpelwar <rani@8thlight.com>
2018-06-13 09:14:57 -0400

    Refactor function to be a class for rendering submit buttons

---
+++ public/common/submit_button_presenter.js
@@ -0,0 +1,11 @@
+class SubmitButtonPresenter {
+  render(parentElement, button_id, button_text) {
+    let button = document.createElement("button");
+    button.setAttribute("type", "submit");
+    button.setAttribute("id", button_id);
+    let text = document.createTextNode(button_text);
+    button.appendChild(text);
+    parentElement.appendChild(button);
+    return button;
+  }
+}
