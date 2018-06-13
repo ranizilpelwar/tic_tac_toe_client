232e262c61ad5779b068b6fc80b52d279e8d0c98
Rani Zilpelwar <rani@8thlight.com>
2018-06-13 15:57:19 -0400

    Will remove existing exceptions if exists

--- public/exceptions/exceptions_presenter.js
+++ public/exceptions/exceptions_presenter.js
@@ -1,8 +1,13 @@
 class ExceptionsPresenter {
-  render(parentElement, exception_text) {
+  render(parentElement, exceptionType, exceptionText) {
+    let existingException = document.getElementById("exception_content");
+    if (existingException !== null) {
+      RemoveElements.at(existingException);
+    }
     let div = document.createElement("div");
-    div.setAttribute("id", "exceptions");
-    let text = document.createTextNode(exception_text);
+    div.setAttribute("id", "exception_content");
+    let text = document.createTextNode(exceptionText);
+    console.log("Exception: " + exceptionType + ", Exception Details: " + exceptionText);
     div.appendChild(text);
     parentElement.appendChild(div);
   }
