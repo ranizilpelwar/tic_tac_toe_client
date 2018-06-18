ad547af1a10340e53f4eca2b69fdfdf22a740a0b
Rani Zilpelwar <rani@8thlight.com>
2018-06-18 16:35:32 -0400

    Add more tests to verify methods in render are called

---
+++ jasmine/spec/player_tile_selection_presenter_spec.js
@@ -0,0 +1,37 @@
+describe("A PlayerTileSelectionPresenter", function() {
+  describe("method called render", function() {
+    it("calls setTimeout when the current player is a Human", function() {
+      let presenter = new PlayerTileSelectionPresenter;
+      let gameDetails = {
+        "game": {
+          "language_tag": "en",
+          "record_moves": false,
+          "match_number": 2,
+          "board": ["1", "2", "3", "4", "5", "6", "7", "8", "9"],
+          "last_move_for_player1": -1,
+          "last_move_for_player2": -1
+        }, 
+        "errors": {
+          "error_message": ""
+        }
+      };
+
+      let game = new Game(gameDetails);
+
+      setTimeout = jasmine.createSpy("setTimeout");
+      
+      let gameSetupData = {
+        "matchNumber": 2,
+        "firstPlayerSymbol": "X",
+        "secondPlayerSymbol": "Y",
+        "selectedFirstPlayerSymbol": "X"
+      };
+
+      let players = new Players(gameSetupData);
+      let div = document.createElement("div");
+      presenter.render(div, gameDetails, players);
+
+      expect(setTimeout).toHaveBeenCalled();
+    });
+  });
+});
