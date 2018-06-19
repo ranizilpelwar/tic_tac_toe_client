9ba1b42d7d45866a3ca4a111d3d315930decf51a
Rani Zilpelwar <rani@8thlight.com>
2018-06-19 10:03:36 -0400

    Refactor to splice out actions into functions that can be spied upon to confirm logic

--- public/game/end_game_state_presenter.js
+++ public/game/end_game_state_presenter.js
@@ -1,14 +1,28 @@
 class EndGameStatePresenter {
-  render(parentElement, gameDetails, players) {
-    let text = new TextPresenter;
-    text.render(parentElement, applicationMessages["messages"]["game_over"]);
+  render(parentElement, gameDetails) {
+    this.displayGameOverText(parentElement);
     if (gameDetails["statuses"]["tie_game"] === true){
-      text.render(parentElement, applicationMessages["messages"]["tie_game"]);
+      this.displayTieGameText(parentElement);
     }
     else {
-      let winnerTemplate = applicationMessages["messages"]["player_won"]
-      let updatedMessageText = winnerTemplate.replace("[1]", gameDetails["statuses"]["winner"].toUpperCase());
-      text.render(parentElement, updatedMessageText);
+      this.displayWinnerText(parentElement, gameDetails);
     }
+  }
+
+  displayGameOverText(parentElement) {
+    let text = new TextPresenter;
+    text.render(parentElement, applicationMessages["messages"]["game_over"]);
+  }
+
+  displayTieGameText(parentElement) {
+    let text = new TextPresenter;
+    text.render(parentElement, applicationMessages["messages"]["tie_game"]);
+  }
+
+  displayWinnerText(parentElement, gameDetails) {
+    let text = new TextPresenter;
+    let winnerTemplate = applicationMessages["messages"]["player_won"]
+    let updatedMessageText = winnerTemplate.replace("[1]", gameDetails["statuses"]["winner"].toUpperCase());
+    text.render(parentElement, updatedMessageText);
   }
 }
