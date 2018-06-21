b5eb8426e6c5cbaaa75bae91c35c6d8c45102ecf
Rani Zilpelwar <rani@8thlight.com>
2018-06-21 09:09:13 -0400

    Disabled promise test for right now

--- jasmine/spec/request_coordinator_promise_spec.js
+++ jasmine/spec/request_coordinator_promise_spec.js
@@ -17,6 +17,22 @@ describe("A Request Coordinator", function() {
       requestCoordinator.get("/message_content");
       expect(MockXHR.prototype.send).toHaveBeenCalled();
     });
+
+  //   it("should return a Promise", function(done) {
+  //     let callback = function() {return new MockXHR("", 200)};
+  //     let requestCoordinator = new RequestCoordinator(callback);
+  //     spyOn(requestCoordinator, 'get').and.callThrough();
+
+  //     // requestCoordinator.get("/message_content")
+  //     //   .then(function(result){
+  //     //     expect(result).toBe(true);
+  //     //     done();
+  //     //  });
+
+  //     let output = requestCoordinator.get("/message_content");
+  //     expect(output).toEqual(jasmine.any(Promise));
+
+  //   });
   });
 
   describe("put function", function() {
