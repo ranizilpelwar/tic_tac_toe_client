8dd8920802f200f9f9ae62cc7175b27113aded40
Rani Zilpelwar <rani@8thlight.com>
2018-06-25 12:40:02 -0400

    Modify url to point to heroku url

--- public/request/uri_generator.js
+++ public/request/uri_generator.js
@@ -1,6 +1,6 @@
 class URIGenerator {
   constructor() {
-    let site = "http://localhost:4567";
+    let site = "https://murmuring-coast-64789.herokuapp.com";
     this.getSite = site;
     this.uri = function(route_string) {
       let uri = this.getSite + route_string;
