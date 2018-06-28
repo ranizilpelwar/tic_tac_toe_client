84616a13fffaa3fb1f7406dd2ec2ffa5bfb0ff28
Rani Zilpelwar <rani@8thlight.com>
2018-06-28 13:05:28 -0400

    Update gemfile lock

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,8 @@
 
 set :application, "tic_tac_toe_client"
 set :repo_url, "git@github.com:ranizilpelwar/tic_tac_toe_client.git"
-set :deploy_to, "/var/www/tic_tac_toe_client"
+#set :deploy_to, "/var/www/tic_tac_toe_client"
+set :deploy_to, "/var/www/html"
 set :ssh_options, {
   forward_agent: true,
   auth_methods: %w[publickey],
