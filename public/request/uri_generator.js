e12f9ec2b682cb86cde965eb64e9596fecc0c830
Rani Zilpelwar <rani@8thlight.com>
2018-06-29 09:59:32 -0400

    Use EC2 instance for web server instead of Heroku

--- public/request/uri_generator.js
+++ public/request/uri_generator.js
@@ -1,6 +1,7 @@
 class URIGenerator {
   constructor() {
-    let site = "https://murmuring-coast-64789.herokuapp.com";
+//  let site = "https://murmuring-coast-64789.herokuapp.com";
+    let site = "http://ec2-18-191-247-154.us-east-2.compute.amazonaws.com/";
     this.getSite = site;
     this.uri = function(route_string) {
       let uri = this.getSite + route_string;
