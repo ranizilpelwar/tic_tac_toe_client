56b1c10d4b31c94275d5614c6a2e9421c72f192b
Rani Zilpelwar <rani@8thlight.com>
2018-07-09 16:28:54 -0400

    Use web server living on Heroku

--- public/request/uri_generator.js
+++ public/request/uri_generator.js
@@ -1,7 +1,6 @@
 class URIGenerator {
   constructor() {
-//  let site = "https://murmuring-coast-64789.herokuapp.com";
-    let site = "http://ec2-18-191-247-154.us-east-2.compute.amazonaws.com";
+    let site = "https://murmuring-coast-64789.herokuapp.com";
     this.getSite = site;
     this.uri = function(route_string) {
       let uri = this.getSite + route_string;
